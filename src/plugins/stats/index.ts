131967569666a3eeb9cf7f55fe04b95f1f76d35b
Felipe Plets <felipe.plets@zensurance.com>
2023-04-22 11:29:00 -0700

    feat: add first version of the stats command

---
+++ src/plugins/stats/index.ts
@@ -0,0 +1,62 @@
+import fs from "fs"
+import path from "path"
+
+function findPackageJsonFiles(directoryPath: string): string[] {
+  const files = fs.readdirSync(directoryPath)
+
+  let packageJsonFiles: string[] = []
+
+  files.forEach((file) => {
+    const filePath = path.join(directoryPath, file)
+    const stats = fs.statSync(filePath)
+
+    if (stats.isDirectory() && file !== "node_modules") {
+      packageJsonFiles = packageJsonFiles.concat(findPackageJsonFiles(filePath))
+    } else if (stats.isFile() && file === "package.json") {
+      packageJsonFiles.push(filePath)
+    }
+  })
+
+  return packageJsonFiles
+}
+
+type Result = { file: string; value: string }
+type Results = [Result?]
+
+function findPackageJsons(
+  attribute: string,
+  callback: (results: Results) => void,
+) {
+  const packageJsonFiles = findPackageJsonFiles(process.cwd())
+
+  const results: Results = []
+
+  packageJsonFiles.forEach((file) => {
+    const packageJson = JSON.parse(fs.readFileSync(file, "utf-8"))
+    const value = packageJson[attribute]
+
+    if (value) {
+      results.push({ file, value })
+    }
+  })
+
+  callback(results)
+}
+
+findPackageJsons("name", (results) => {
+  // results.forEach(result => {
+  // console.log(results)
+  // console.log("Packages: ", results.length)
+  // console.log("Packages without prefix: ", results.filter(r => !r.value.startsWith("@zensurance")).length)
+  console.log(
+    "Packages not in domains: ",
+    results.filter((r) => !r.file.includes("/domains")).length,
+  )
+  // console.log(results.filter(r => !r.file.includes("/domains")))
+  results
+    .filter((r) => r.file.includes("/domains") && !r.value.startsWith("@zens"))
+    // .map(r => r.file.replace("/Users/plets/projects/zen/zen/", ""))
+    .forEach((r) => console.log(r))
+
+  // })
+})
